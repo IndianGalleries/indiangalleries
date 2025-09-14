82983aa5d225d4115d0191274f02e2b3b98fc067
monikamaurya <monikamaurya2611@gmail.com>
2025-09-14 18:41:55 +0530

    Create a new page for color swatches and create commonheaderfooter page

---
+++ js-files/commonheaderfooter.js
@@ -0,0 +1,16 @@
+// commonheaderfooter.js
+async function loadComponent(id, file) {
+  try {
+    const res = await fetch(file);
+    if (!res.ok) throw new Error(`Failed to load ${file}`);
+    const html = await res.text();
+    document.getElementById(id).innerHTML = html;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  loadComponent("header", "header.html");
+  loadComponent("footer", "footer.html");
+});
